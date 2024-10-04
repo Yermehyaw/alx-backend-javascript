2281166db915dcb4dd6f570f8eb963b8ae1a3ac0
Yermehyaw <yermehyaw@gmail.com>
2024-10-04 11:28:49 +0000

    Alter variable types

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*
+ * Modify const and let vars
+ */
+
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
