5f70c813d6148471d8763ec727084049fe519a8a
Yermehyaw <yermehyaw@gmail.com>
2024-10-06 14:18:27 -0700

    Use an if/else to handle promise resolution or failure

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    const newObj = {
+      status: 200;
+      body: 'Success';
+    };
+    if (success) {
+      resolve(newObj);
+    } else {
+      reject(console.log('The fake API is not working currently')):
+    }
+  )};
+}
