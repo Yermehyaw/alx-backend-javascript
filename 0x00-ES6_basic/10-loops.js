587822edd57480c4fca2ee0f3b4d4f438d95b953
Yermehyaw <yermehyaw@gmail.com>
2024-10-06 01:49:52 +0000

    Fix eslint errs

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -3,7 +3,7 @@ export default function appendToEachArrayValue(array, appendString) {
   for (let value of array) {
     array[idx] = appendString + value;
     ++idx;
-  }
+  };
 
   return array;
 }
