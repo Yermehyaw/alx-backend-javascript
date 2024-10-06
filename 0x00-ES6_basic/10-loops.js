404772fee7c709c001549c09eba6d17891691f31
Yermehyaw <yermehyaw@gmail.com>
2024-10-06 01:14:25 +0000

    Use let and var;

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array.keys()) {
-    const value = array[idx];
+  for (let idx of array.keys()) {
+    let value = array[idx];
     array[idx] = appendString + value;
   }
 
