c1ae454b6d854dd92b39e9fd26c7491bdc1ed790
Yermehyaw <yermehyaw@gmail.com>
2024-10-06 03:43:39 +0000

    Add shorthand for array expansion

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,10 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
   const allEmployees = {
-    `${departmentName}`: [
-      for (const value of employees) {
-	return value;
-      }
-      // `${employees.values()}
+    [departmentName]: employees;
     ]
   };
 
