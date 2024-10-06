bc6b6b935eafd9067a319600eb6e5b0e535c96fb
Yermehyaw <yermehyaw@gmail.com>
2024-10-06 03:47:18 +0000

    Fix typo

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
   const allEmployees = {
-    [departmentName]: employees;
-    ]
+    [departmentName]: employees
   };
 
   return allEmployees;
