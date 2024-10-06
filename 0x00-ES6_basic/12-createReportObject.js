66ac3a51c6ac19b599257b3231625058c582f0f6
Yermehyaw <yermehyaw@gmail.com>
2024-10-06 11:39:33 +0000

    Define an obj that uses spread syntax and for..of loop

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,17 @@
 export default function createReportObject(employeesList) {
-  
+  const officeReport = {
+    allEmployees:  { ...employeesList };
+
+    getNumberOfDepartments(allEmployeesProperty) {
+      // return no of keys in employeesList
+      let obj_no = 0;
+      for (const value of allEmployeesProperty) {
+	obj_no += 1;
+      };
+
+      return obj_no;
+    },
+  };
+
+  return officeRepprt
 }
