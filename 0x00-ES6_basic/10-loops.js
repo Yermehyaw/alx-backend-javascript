4d5fd3ea125f566b8872b98b7c1e0faa509365f6
Yermehyaw <yermehyaw@gmail.com>
2024-10-06 01:13:24 +0000

    Use const variables

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx of array.keys()) {
-    var value = array[idx];
+  for (const idx of array.keys()) {
+    const value = array[idx];
     array[idx] = appendString + value;
   }
 
