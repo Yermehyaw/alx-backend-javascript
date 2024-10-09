183a660223a3cce9bf93febd545025a74be0be99
Yermehyaw <yermehyaw@gmail.com>
2024-10-09 02:36:37 -0700

    Fix eslint err & restruct code

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,17 +1,19 @@
 export default function createInt8TypedArray(length, position, value) {
   const newBuffer = new ArrayBuffer(length);
 
-  const int8View = new Int8Array(newBuffer);  // create a view obj to read and write to the array buffer
+  const DataView = new Int8Array(newBuffer); // create a view obj to read/write to the array buffer
 
   if (position > length - 1) {
     throw new Error('Position outside range');
   } else {
-    int8View[position] = value;
+    DataView[position] = value;
   }
 
-  return {
-    byteLength: int8View.length,
-    byteOffset: int8View.byteOffset,
+  const bufferProperties = {
+    byteLength: DataView.length,
+    byteOffset: DataView.byteOffset,
     buffer: newBuffer,
-  }
+  };
+
+  return bufferProperties;
 }
