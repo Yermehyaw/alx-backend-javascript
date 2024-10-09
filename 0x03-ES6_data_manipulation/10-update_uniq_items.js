21500eca1d81e6066015a3d7b678452dc1daf1ab
Yermehyaw <yermehyaw@gmail.com>
2024-10-09 04:53:58 -0700

    Fix eslint errs

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -4,15 +4,15 @@
 
 export default function updateUniqueItems(map) {
   // ascertain map is a Map() obj
-  if (!(map instanceof Map )) {
+  if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
 
-  for ( const [key, value] of map.entries() ) {
+  for (const [key, value] of map.entries()) {
     if (value === 1) {
       try {
         map.set(key, 100);
-      } catch(error) {
+      } catch (error) {
         throw new Error('Cannot process');
       }
     }
