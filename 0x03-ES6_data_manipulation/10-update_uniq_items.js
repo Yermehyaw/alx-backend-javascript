d110b25f91422bf50435c627d979f4ed8feab20a
Yermehyaw <yermehyaw@gmail.com>
2024-10-09 04:51:42 -0700

    Update a map obj

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,20 @@
+/*
+ * Returns an updated map for all items with initial quantity at 1
+ */
+
+export default function updateUniqueItems(map) {
+  // ascertain map is a Map() obj
+  if (!(map instanceof Map )) {
+    throw new Error('Cannot process');
+  }
+
+  for ( const [key, value] of map.entries() ) {
+    if (value === 1) {
+      try {
+        map.set(key, 100);
+      } catch(error) {
+        throw new Error('Cannot process');
+      }
+    }
+  }
+}
