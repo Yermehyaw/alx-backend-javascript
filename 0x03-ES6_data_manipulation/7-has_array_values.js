fc8912d44a60d0d5729c9d264edf52448c651cb3
Yermehyaw <yermehyaw@gmail.com>
2024-10-09 03:23:35 -0700

    Fix eslint errs

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -2,8 +2,9 @@ export default function hasValuesFromArray(set, array) {
   const newSet = new Set(array);
 
   for (const elem of newSet) {
-    if (!(set.has(elem)))
+    if (!(set.has(elem))) {
       return false;
+    }
   }
 
   return true;
