9599f6263c8ddb021d6638e154bc44d0635483ba
Yermehyaw <yermehyaw@gmail.com>
2024-10-09 15:09:59 -0700

    Create a table of type scripted array of typed objects

---
+++ 0x04-TypeScript/task_0/main.ts
@@ -0,0 +1,39 @@
+/**
+ * Creating an interface for a student
+ */
+
+Interface Student {
+  firstName: string,
+  lastName: string,
+  age: number,
+  location: string
+}
+
+const student_1: Student = {
+  firstName: 'Emmanuel',
+  lastName: 'John',
+  age: 20,
+  location: 'Lagos',
+};
+const student_2: Student = {
+  firstName: 'Samuel',
+  lastName: 'John',
+  age: 17,
+  location: 'Abuja',
+};
+
+const studentList: array = [student_1, student_2];
+
+// Render a table
+const studentTable = document.createElement('table');
+studentList.forEach((student) => {
+  const row = studentTable.insertRow();
+  const firstNameCol = row.insertCell(0);
+  const locationCol = row.insertCell(1);
+  firstNameCol.textContent(student.firstName);
+  locationCol.textContent(student.location);
+});
+
+// Add table to html doc
+document.body.appendChild(studentTable);
+
