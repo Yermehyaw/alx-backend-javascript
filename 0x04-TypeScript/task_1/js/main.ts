422bd6c646c34718b147b047906dedc069aae32c
Yermehyaw <yermehyaw@gmail.com>
2024-10-10 01:13:12 +0100

    Add a typed class with class and const ructor descriptors/interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,3 +14,32 @@ interface Directors extends Teacher<any> {
 function printTeacher(firstName: string, lastName: string): string  {
   return `${firstName[0]}. ${lastName}`;
 }
+
+interface Student {
+  firstName: string,
+  lastName: string,
+  workOnHomework(): string,
+  displayName(): string,
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): Student;
+}
+
+class StudentClass implements Student {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  } 
+}
