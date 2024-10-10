6ed32aa7277cfb0991d71c3718b20c0029bdd35e
Yermehyaw <yermehyaw@gmail.com>
2024-10-10 09:58:13 -0700

    Fix code struct

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,10 +3,11 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   let countEndpointCall = weakMap.get(endpoint) || 0; // no of times a spec endpoint is called
 
+  countEndpointCall += 1;
+
   if (countEndpointCall >= 5) {
     throw new Error('Endpoint load is high');
   }
 
-  countEndpointCall += 1;
   weakMap.set(endpoint, countEndpointCall);
 }
