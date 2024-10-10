9519fad174184b4c2d4b569514afdec15c91f97b
Yermehyaw <yermehyaw@gmail.com>
2024-10-10 09:45:51 -0700

    Track an object(endpoint) proprty no of times called

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+export const weakMap = new WeakMap();
+
+export default function queryAPI(endpoint) {
+  let countEndpointCall = weakMap.get(endpoint) || 0; // no of times a spec endpoint is called
+
+  if (countEndpointCall >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  countEndpointCall += 1;
+  weakMap.set(endpoint, countEndpointCall);
+}
