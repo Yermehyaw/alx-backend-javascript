5533a3348dfe64fd16ec15d69b8ff9fa74d9426b
Yermehyaw <yermehyaw@gmail.com>
2024-10-10 11:29:58 +0100

    Update ambience declarartion sto crud.js funcs

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -4,8 +4,8 @@
 import RowID from './interface.ts';
 import RowElement from './interface.ts';
 
-declare function insertRow(row: number): RowID;
+declare function insertRow(row: RowElement): RowID;
 
 declare function deleteRow(rowId: RowID): void;
 
-declare function updateRow(rowId: RowID, row: number): RowID;
+declare function updateRow(rowId: RowID, row: RowElement): RowID;
