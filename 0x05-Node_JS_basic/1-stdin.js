83cb046841ad2c924823cdcb5cdc4520492a78b0
Yermehyaw <yermehyaw@gmail.com>
2024-11-27 19:44:26 -0800

    Fix lint errs

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@
  */
 
 // Receive user's name
-process.stdout.write('Welcome to Holberton School, what is your name?' + '\n')
+process.stdout.write('Welcome to Holberton School, what is your name?' + '\n');
 
 // Set enconding
 process.stdin.setEncoding('utf-8');
