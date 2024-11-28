5e024c6337273b40a6be5f8336d95dd33c342618
Yermehyaw <yermehyaw@gmail.com>
2024-11-27 19:41:40 -0800

    Receive input frm user via tty

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,24 @@
+/**
+ * Utilizing process module for receiving inputs and dosplaying
+ * backend outputs to user
+ */
+
+// Receive user's name
+process.stdout.write('Welcome to Holberton School, what is your name?' + '\n')
+
+// Set enconding
+process.stdin.setEncoding('utf-8');
+
+// Print user's name
+process.stdin.on('data', (name) => {
+  process.stdout.write('Your name is: ' + name.trim() + '\n');
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing' + '\n');
+  process.exit(0);
+});
+
+process.on('SIGINT', () => {
+  process.stdout.write('This important software is now closing' + '\n');
+});
