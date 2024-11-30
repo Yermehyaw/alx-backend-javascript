6db6ea2fd74761a92f3a7b0dd1853779d904773a
Yermehyaw <yermehyaw@gmail.com>
2024-11-30 13:42:29 -0800

    Fix to use module.exports

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,44 +1,42 @@
 /**
  * Receive input data and parse synchrnously
  */
-const fs = require('fs');
+const countStudents = (filePath) => {
+  
+  const fs = require('fs');
 
-const args = process.argv;
-if (!args[2]) {
-  throw new Error('Cannot load the database');
-}
+  try {
+    // Open file
+    const fd = fs.openSync(filePath, 'r');
 
-const filePath = args[2];
-
-try {
-  // Open file
-  const fd = fs.openSync(filePath, 'r');
-
-  // Read data from file descriptor
-  const buffer = Buffer.alloc(250); // create buffer for read storage
-  const bytesRead = fs.readSync(fd, buffer, 0, buffer.length, 0);
-  // parse the read data stored in buffer
-  const dataArr = buffer.toString('utf8', 0, bytesRead).split('\n');
-  const noStudents = dataArr.length - 1;
-  const csStudents = [];
+    // Read data from file descriptor
+    const buffer = Buffer.alloc(250); // create buffer for read storage
+    const bytesRead = fs.readSync(fd, buffer, 0, buffer.length, 0);
+    // parse the read data stored in buffer
+    const dataArr = buffer.toString('utf8', 0, bytesRead).split('\n');
+    const noStudents = dataArr.length - 1;
+    const csStudents = [];
   const sweStudents = [];
 
-  for (let idx = 1; idx < dataArr.length; idx += 1) {
-    const studentDetails = dataArr[idx].split(',');
-    if (studentDetails[3] === 'CS') {
-      csStudents.push(studentDetails[0]);
-    } else if (studentDetails[3] === 'SWE') {
-      sweStudents.push(studentDetails[0]);
+    for (let idx = 1; idx < dataArr.length; idx += 1) {
+      const studentDetails = dataArr[idx].split(',');
+      if (studentDetails[3] === 'CS') {
+	csStudents.push(studentDetails[0]);
+      } else if (studentDetails[3] === 'SWE') {
+	sweStudents.push(studentDetails[0]);
+      }
     }
+
+    // print parsed data
+    console.log(`No of students: ${noStudents}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+
+    // Close file after reading
+    fs.closeSync(fd);
+  } catch (err) {
+    throw new Error(`Cannot load the database`);
   }
+};
 
-  // print parsed data
-  console.log(`No of students: ${noStudents}`);
-  console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
-  console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-
-  // Close file after reading
-  fs.closeSync(fd);
-} catch (err) {
-  throw new Error(`Cannot load the database: ${err}`);
-}
+module.exports = countStudents;
